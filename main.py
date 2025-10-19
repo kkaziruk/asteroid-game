1e3d80ba8f1d9f5d27a7e24b02ee8695e6ceeeb0
Kaza-Zack Kazirukanyo <kkaziruk@gmail.com>
2025-10-19 18:25:06 -0400

    add latest files

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # create an object to help track time
+    clock = pygame.time.Clock()
+    dt = 0
     # creating a surface, 'screen'
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     # choosing a black colour to fill
-    color = (0, 0, 0)
+    color = (0, 0, 0)   
+    # creating groups
+    updatable = []
+    drawable = []
+    Player.containers = (updatable, drawable)
+    # instantiate player object 
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT
 
     while True:
-        # filling color to the screen
+        # the program will never run at more than 60 frames per second
+        # clock.tick(60) returns the number of milliseconds since the last tick call
+        # dt is the number of seconds for one frame
+        dt = clock.tick(60)/1000
+        # update background
         screen.fill(color)
-        # updating the display
-        pygame.display.flip()
-
+        # rotate image
+        for obj in updatables:
+            obj.update(dt)
+        # draw triangle
+        for drw in drawables:
+            drw.draw(screen)
+        # event handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # update display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
